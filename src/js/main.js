a6ba47874023fb023a372e59e16c8cdee5e9bf91
NataliaVozhdaeva <LomzNat@yandex.ru>
2022-10-30 01:15:36 +0200

    done decktop version layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,24 @@
 const cards = document.querySelectorAll('.card');
+const startTimesList = document.querySelector('.starttime');
 let startTimes = [];
+const moreTimeBtns = document.querySelectorAll('.starttime-item--showmore');
+const showHiddenEls = () => {
+  for (let i = 3; i < startTimes.length; i++) {
+    startTimes[i].classList.toggle('non-displayed');
+  }
+};
 
 for (let card of cards) {
   startTimes = card.querySelectorAll('.starttime-item');
-  if (startTimes.length > 4) {
-    for (let i = 0; i < startTimes.length; i++) {
-      startTimes[3].querySelector('.starttime-btn').textContent = 'ещё...';
-      startTimes[3].querySelector('.starttime-btn').classList.add('showMore');
-      startTimes[4].classList.add('non-displayed');
-    }
-
-    const moreTimeBtn = card.querySelector('.showMore');
-    moreTimeBtn.addEventListener('click', () => console.log('click'));
+  if (startTimes.length > 5) {
+    showHiddenEls();
   }
+}
+
+for (let moreTimeBtn of moreTimeBtns) {
+  moreTimeBtn.addEventListener('click', (e) => {
+    startTimes =
+      e.target.parentNode.parentNode.querySelectorAll('.starttime-item');
+    showHiddenEls();
+  });
 }
